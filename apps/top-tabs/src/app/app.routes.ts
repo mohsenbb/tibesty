e1e21c9cb170e6ccad90996410a0e9d0049a4c47
mohsen madi <mohsen@backbase.com>
2022-10-31 00:36:35 +0100

    working on routes

---
+++ apps/top-tabs/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Route } from '@angular/router';
+
+export const appRoutes: Route[] = [
+];
+
+// next:
+//  - create two baby comps
+//  - route to them
+//  works?
+//  - change comp to app (https://github.com/nrwl/nx-examples/tree/master/apps/products)
