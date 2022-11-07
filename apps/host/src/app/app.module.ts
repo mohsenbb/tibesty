a8e31c5caa9b2c853a094e54c4ee214cadc9be57
mohsen madi <mohsen@backbase.com>
2022-11-07 14:40:38 -0500

    material complete

--- apps/host/src/app/app.module.ts
+++ apps/host/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialModule } from "@tibesty/material";
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    BrowserAnimationsModule,
+    MaterialModule,
+    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' })
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ libs/material/src/lib/material.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+
+@NgModule({
+  exports: [
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
+  ]
+})
+export class MaterialModule {
+}
